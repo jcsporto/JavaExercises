3481430e95097839509598657e6c8ee2d71297ef
Jean Porto <portojean@gmail.com>
2022-04-22 17:28:46 -0300

    atualizando class Program

--- Account/src/application/Program.java
+++ Account/src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		//UPCASTING
+		Account acc1 = bacc;
+		
 	}
 
 }
